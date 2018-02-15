b07e9005b0942d9ade9731c4c1a0344802ed2715
sudha <sudha@sudha-PC>
2018-02-15 20:55:56 +0530

    Commit5

---
+++ src/test/java/com/testone/selenium/Flightbooking.java
@@ -0,0 +1,19 @@
+package com.testone.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class Flightbooking 
+{
+	@Test
+	public static void verify()
+	{
+		
+		System.out.println("Hello welcome to jenkins");
+					
+	}
+	
+
+}
